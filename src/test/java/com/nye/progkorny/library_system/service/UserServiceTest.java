d433e2f8a8c588b0e70443db6f49a4182a06d091
Laskovics Lilla <laskovicslillamaria@gmail.com>
2026-05-11 14:21:05 +0200

    Moved to a new package

---
+++ src/test/java/com/nye/progkorny/library_system/service/UserServiceTest.java
@@ -0,0 +1,38 @@
+package com.nye.progkorny.library_system.service;
+
+import com.nye.progkorny.library_system.model.User;
+import com.nye.progkorny.library_system.repository.UserRepository;
+import com.nye.progkorny.library_system.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    void shouldReturnAllUsers() {
+        User user = new User();
+        user.setName("Test");
+
+        when(repository.findAll()).thenReturn(List.of(user));
+
+        List<User> result = service.getAllUsers();
+
+        assertEquals(1, result.size());
+        assertEquals("Test", result.get(0).getName());
+    }
+}
