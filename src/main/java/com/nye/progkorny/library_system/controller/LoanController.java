b5ca7623d4b8ef00ed257609e17cbd47ea008969
Laskovics Lilla <laskovicslillamaria@gmail.com>
2026-05-17 02:07:24 +0200

    Backend fix

--- src/main/java/com/nye/progkorny/library_system/controller/LoanController.java
+++ src/main/java/com/nye/progkorny/library_system/controller/LoanController.java
@@ -2,7 +2,9 @@ package com.nye.progkorny.library_system.controller;
 
 import com.nye.progkorny.library_system.model.Loan;
 import com.nye.progkorny.library_system.service.LoanService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -22,10 +24,29 @@ public class LoanController {
         return loanService.getAllLoans();
     }
 
-    @PostMapping
-    public Loan createLoan(@RequestBody Map<String, Long> body) {
-        return loanService.createLoan(body.get("userId"), body.get("bookId"));
+//    @PostMapping
+//    public Loan createLoan(@RequestBody Map<String, Long> body) {
+//        return loanService.createLoan(body.get("userId"), body.get("bookId"));
+//    }
+@PostMapping
+public ResponseEntity<?> createLoan(@RequestBody Map<String, Long> body) {
+
+    try {
+
+        Loan loan = loanService.createLoan(
+                body.get("userId"),
+                body.get("bookId")
+        );
+
+        return ResponseEntity.ok("Sikeres kölcsönzés!");
+
+    } catch (ResponseStatusException e) {
+
+        return ResponseEntity
+                .badRequest()
+                .body(e.getReason());
     }
+}
 
     @DeleteMapping("/{id}")
     public void deleteLoan(@PathVariable Long id) {
